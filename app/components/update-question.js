ea3424e0ba2785d620c9979caf84d68a55d9dbcb
Anand Angalig <anandangalig@gmail.com>
2016-11-04 11:10:52 -0400

    add update-question component

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateFormShowing: false,
+  actions: {
+    showUpdateForm() {
+      this.set('updateFormShowing', true);
+    },
+    showUpdateFormHide() {
+      this.set('updateFormShowing', false);
+    },
+
+    updateQuestion(question) {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes')
+      };
+      this.set('updateFormShowing', false);
+
+      //Clear the inputted values after they have been used via forEach loop:
+      var self = this; //fixing the scope issue by creating this var outside of the forEach loop.
+      Object.keys(params).forEach(function(key) { //Object=root object of JS; keys=function that return the keys.
+        self.set(key, "");
+      });
+      this.sendAction('updateQuestion', question, params);
+      //SEND TO 'question' route
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -8,6 +8,16 @@ export default Ember.Route.extend({
     deleteThisQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
+    },
+    updateQuestion(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) { //loops thru the keys of the passed in params, checks if it is not undefined(i.e. had new input to update)
+          question.set(key,params[key]); // params[key]= the Value
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+
     }
   }
 });
