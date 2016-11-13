e911d946362278c08e62fc1c63366e6a01e1982a
Anand Angalig <anandangalig@gmail.com>
2016-11-13 18:02:04 -0500

    work in process: service is done, but need to finish hide/display of the list

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
   notesShowing: false,
   actions: {
     notesToggle: function() {
       var stateOfNotes = this.get('notesShowing');
       this.set('notesShowing', !stateOfNotes);
+    },
+    addToList(question) {
+      this.get('favoritesList').add(question);
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  favoritesList: Ember.inject.service(),
+  showFavorites: false,
   model() {
     return this.store.findAll('question');
   },
@@ -9,6 +11,20 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', parameters);
       newQuestion.save();
       this.transitionTo('index');
+    },
+    // notesToggle: function() {
+    //   var stateOfNotes = this.get('notesShowing');
+    //   this.set('notesShowing', !stateOfNotes);
+    // },
+    showFavoritesList: function() {
+      var stateOfList = this.get('showFavorites');
+      this.set('showFavorites', !stateOfList);
+      console.log(this.get('showFavorites'));
+    },
+
+    hideFavoritesList: function() {
+      var stateOfList = this.get('showFavorites');
+      this.set('showFavorites', !stateOfList);
     }
   }
 });
