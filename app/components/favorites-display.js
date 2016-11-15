43f102ecc5e083feb0c2913e8267e4d5f08c46cd
Anand Angalig <anandangalig@gmail.com>
2016-11-15 15:21:50 -0500

    fix the IF statement by moving it to the favorites-display component

--- app/components/favorites-display.js
+++ app/components/favorites-display.js
@@ -2,5 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service(),
+  showFavorites: false,
+
+  actions: {
+    showFavoritesList: function() {
+      var stateOfList = this.get('showFavorites');
+      // console.log(stateOfList);
+      this.set('showFavorites', !stateOfList);
+      // console.log(this.get('showFavorites'));
+    },
+
+    hideFavoritesList: function() {
+      var stateOfList = this.get('showFavorites');
+      // console.log(stateOfList);
+      this.set('showFavorites', !stateOfList);
+      // console.log(this.get('showFavorites'));
+    }
+  }
 
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   favoritesList: Ember.inject.service(),
-  showFavorites: false,
   model() {
     return this.store.findAll('question');
   },
@@ -11,20 +10,6 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', parameters);
       newQuestion.save();
       this.transitionTo('index');
-    },
-
-    showFavoritesList: function() {
-      var stateOfList = this.get('showFavorites');
-      console.log(stateOfList);
-      this.set('showFavorites', !stateOfList);
-      console.log(this.get('showFavorites'));
-    },
-
-    hideFavoritesList: function() {
-      var stateOfList = this.get('showFavorites');
-      console.log(stateOfList);
-      this.set('showFavorites', !stateOfList);
-      console.log(this.get('showFavorites'));
     }
   }
 });
